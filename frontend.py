8fea076b47dc17dcf8bc5521915b4e6fb3012f5b
Micheal Oluwafemi Adeniyi <160871523+OfemiAdeniyi@users.noreply.github.com>
2026-03-05 21:30:50 +0100

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/screen-diabetes"
+url = "http://127.0.0.1:8000/screen-diabetes"
 # -------------------------------
 # Page Setup
 # -------------------------------
